6d02f596d496b2faed32eb67a51ca176962524ad
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-03-21 11:11:05 +0100

    ww

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php include "php/inculude/header.php"; ?>
+<h1>Welcome to my website</h1>
+
+<?php include "php/inculude/footer.php"; ?>
---
+++ php/include/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script type="text/javascript" src="/dekkerthomas/javascript//bootstrap.min.js"></script>
+    <script type="text/javascript" src="/dekkerthomas/javascript/bootstrap.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/dekkerthomas/javascript/css/bootstrap.min.css">
+    <title>dekkerthomas</title>
+</head>
+<body>
+    
