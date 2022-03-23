9dba4f6e4852082762797bccf29b2fc939fabd1a
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-03-23 14:44:11 +0100

    ww

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php include "php/include/header.php"; ?>
-    
-        <div id="resumebox1" style="inline-block">
-        <div class="typewriter">
-            <h1>Do you want to continue...</h1>
-        </div>
-            <button onclick="resume()" id="resume"><span>yes</span></button>
-            <button onclick="no()" id="no"><span>no</span></button>
-        </div>
+        
+            <div id="resumebox1" style="inline-block">
+                        <div class="typewriter">
+                            <h1>Do you want to continue...</h1>
+                        </div>
+            <div id="boxing">
+                <button onclick="no()" id="no"><span>no</span></button>
+                <button onclick="resume()" id="resume"><span>yes</span></button>
+            </div>
+            </div>
         <iframe id="frame" src="https://en.wikipedia.org/wiki/Platypus" style="display: none;"></iframe>
         <video id="myVideo" style="display: none;">
             <source src="/media/video/intro.mp4" type="video/mp4">
