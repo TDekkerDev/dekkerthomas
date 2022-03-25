a50db2817013e68c6385a59b6a076cb45fd34e4d
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-03-25 11:54:15 +0100

    gg

--- js/buttons.js
+++ js/buttons.js
@@ -1,12 +1,14 @@
 var vid = document.getElementById("myVideo"); 
 function resume(){
-    // document.getElementById("myVideo").style.display = "inline-block";
-    // vid.play(); 
     document.getElementById("boks").classList.add('flicker-in-1');
     setTimeout(function(){
         document.getElementById("no").style.display = "none";
         document.getElementById("resume").style.display = "none";
         document.getElementById("typewriterid").style.display = "none";
+        setTimeout(function(){
+            document.getElementById("myVideo").style.display = "inline-block";
+            vid.play(); 
+        }, 300); 
     }, 3000); 
     
 }
