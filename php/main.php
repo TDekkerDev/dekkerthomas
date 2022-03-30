53a9d77d6efb62135e318738f91a32158953e190
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-03-30 16:41:33 +0200

    WW

--- php/main.php
+++ php/main.php
@@ -1,70 +1,49 @@
-<div class="main">
+<div id="main">
 <?php include "include/header.php"; ?>
   <script>
     AOS.init();
   </script>
 
-<div class="container_header" >
-
-  <div class="Photo_name"  >
-  <img src="/media/img/thomas.jpg" alt="Thomas dekker" id = "thomas">
-  <h1>Thomas Dekker</h1>
-</div>
-</div>
-
-<div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-sine" class="profielschets shadow-diffuse">
-      <article id = "skillsbox">
-        <h2>skills</h2><br>
-        <h3 class = "skillstext">php:</h3><div class = "skillsbar"><div id = "skillsphp"></div></div>
-        <h3 class = "skillstext">javascript:</h3><div class = "skillsbar"><div id = "skillsjs"></div></div>
-        <h3 class = "skillstext">sql:</h3><div class = "skillsbar"><div id = "skillssql"></div></div>
-        <h3 class = "skillstext">c#:</h3><div class = "skillsbar"><div id = "skillsc"></div></div>
-        <h3 class = "skillstext">css:</h3><div class = "skillsbar"><div id = "skillscss"></div></div>
-      </article>
-
-</div>
-
-
-
-
-
-
-
-
-
-  <!-- <div id = "text">
+  <div class="container_header shadow-diffuse" >
+    <div class="container_header_text">
+      <article>
+      <br>
+      <br>
+      Hallo! Ik ben Thomas Dekker.<br>
+      Ik ben op 9 februari 2005 geboren in Obdam.<br>
+      Een van mijn grootste hobby's is zwemmen en Computer.<br>
+      Naast het zwemmen en ben ik ook actief als vrijwilliger.<br>
+      Maar van mijn grootste hobby computer wil ik ook graag mijn beroep maken.<br>
     
-      <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-sine">
-        <article id = "hello">
-          Hello my name is Thomas dekker. <br>
-          I am 17 years old.<br>
-          I am a student at the Horizon colege in hoorn.<br>
-          And I do programming as my education.<br>
-        </article>
-      </div>
 
-        <div data-aos="fade-left"  data-aos-duration="1000" data-aos-easing="ease-in-sine">
-          <article id = "me">
-            this is me:<br>
-            <img src="/media/img/thomas.jpg" alt="Thomas dekker" id = "thomas">
-          </article>
-        </div>
+      </article>
+    </div>
+      <img class="Photoname" src="/media/img/thomas.jpg" alt="Thomas dekker" id = "thomas"><br><br>
+  </div>
 
-        <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-sine">
-          <article id = "skillsbox">
+  <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-sine" class="container_skills shadow-diffuse" >
+    <article>
             <h2>skills</h2><br>
             <h3 class = "skillstext">php:</h3><div class = "skillsbar"><div id = "skillsphp"></div></div>
             <h3 class = "skillstext">javascript:</h3><div class = "skillsbar"><div id = "skillsjs"></div></div>
             <h3 class = "skillstext">sql:</h3><div class = "skillsbar"><div id = "skillssql"></div></div>
             <h3 class = "skillstext">c#:</h3><div class = "skillsbar"><div id = "skillsc"></div></div>
             <h3 class = "skillstext">css:</h3><div class = "skillsbar"><div id = "skillscss"></div></div>
-          </article>
-      </div>
+    </article>
   </div>
-</div> -->
 
 
 
 
+  <!-- <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-sine" class="profielschets shadow-diffuse">
+        <article id = "skillsbox">
+          <h2>skills</h2><br>
+          <h3 class = "skillstext">php:</h3><div class = "skillsbar"><div id = "skillsphp"></div></div>
+          <h3 class = "skillstext">javascript:</h3><div class = "skillsbar"><div id = "skillsjs"></div></div>
+          <h3 class = "skillstext">sql:</h3><div class = "skillsbar"><div id = "skillssql"></div></div>
+          <h3 class = "skillstext">c#:</h3><div class = "skillsbar"><div id = "skillsc"></div></div>
+          <h3 class = "skillstext">css:</h3><div class = "skillsbar"><div id = "skillscss"></div></div>
+        </article>
+  </div> -->
 
 <?php include "include/footer.php"; ?>
