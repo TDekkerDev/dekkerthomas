6a23d0991f9790e5bb9b1d01afad67fa9f5cb041
thomas <46562812+Thomas123456789bobby@users.noreply.github.com>
2022-03-30 13:16:08 +0200

    w

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dekkerthomas
 https://michalsnik.github.io/aos/
 https://getbootstrap.com/
+https://fonts.google.com/specimen/IBM+Plex+Sans+Arabic
+https://colorhunt.co/palette/1919192d4263c84b31ecdbba
--- php/main.php
+++ php/main.php
@@ -1,35 +1,37 @@
 <?php include "include/header.php"; ?>
-<script>
-  AOS.init();
-</script>
-<div id = "text">
-  
-    <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-sine">
-      <article id = "hello">
-        Hello my name is Thomas dekker. <br>
-        I am 17 years old.<br>
-        I am a student at the Horizon colege in hoorn.<br>
-        And I do programming as my education.<br>
-      </article>
-    </div>
-
-      <div data-aos="fade-left"  data-aos-duration="1000" data-aos-easing="ease-in-sine">
-        <article id = "me">
-          this is me:<br>
-          <img src="/media/img/thomas.jpg" alt="Thomas dekker" id = "thomas">
+<div class="main">
+  <script>
+    AOS.init();
+  </script>
+  <div id = "text">
+    
+      <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-sine">
+        <article id = "hello">
+          Hello my name is Thomas dekker. <br>
+          I am 17 years old.<br>
+          I am a student at the Horizon colege in hoorn.<br>
+          And I do programming as my education.<br>
         </article>
       </div>
 
-      <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-sine">
-        <article id = "skillsbox">
-          <h2>skills</h2><br>
-          <h3 class = "skillstext">php:</h3><div class = "skillsbar"><div id = "skillsphp"></div></div>
-          <h3 class = "skillstext">javascript:</h3><div class = "skillsbar"><div id = "skillsjs"></div></div>
-          <h3 class = "skillstext">sql:</h3><div class = "skillsbar"><div id = "skillssql"></div></div>
-          <h3 class = "skillstext">c#:</h3><div class = "skillsbar"><div id = "skillsc"></div></div>
-          <h3 class = "skillstext">css:</h3><div class = "skillsbar"><div id = "skillscss"></div></div>
-        </article>
-    </div>
+        <div data-aos="fade-left"  data-aos-duration="1000" data-aos-easing="ease-in-sine">
+          <article id = "me">
+            this is me:<br>
+            <img src="/media/img/thomas.jpg" alt="Thomas dekker" id = "thomas">
+          </article>
+        </div>
+
+        <div data-aos="fade-right"  data-aos-duration="1000" data-aos-easing="ease-in-sine">
+          <article id = "skillsbox">
+            <h2>skills</h2><br>
+            <h3 class = "skillstext">php:</h3><div class = "skillsbar"><div id = "skillsphp"></div></div>
+            <h3 class = "skillstext">javascript:</h3><div class = "skillsbar"><div id = "skillsjs"></div></div>
+            <h3 class = "skillstext">sql:</h3><div class = "skillsbar"><div id = "skillssql"></div></div>
+            <h3 class = "skillstext">c#:</h3><div class = "skillsbar"><div id = "skillsc"></div></div>
+            <h3 class = "skillstext">css:</h3><div class = "skillsbar"><div id = "skillscss"></div></div>
+          </article>
+      </div>
+  </div>
 </div>
 
 
